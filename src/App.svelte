7855d2b1835ed5729c42b24c19ff35e5cb1bc267
haivu2001 <29654375+haivu2001@users.noreply.github.com>
2021-05-21 20:12:52 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<div class="view">
+    <header>header</header>
+    <main>hello</main>
+    <footer>hello</footer>
+</div>
+
+<style>
+    @media (min-width: 640px) {
+        .view {
+            max-width: 640px;
+        }
+    }
+
+    .view {
+        background: white;
+        width: 100%;
+        height: 100%;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        justify-content: stretch;
+        position: relative;
+    }
+
+    .view > * {
+        flex-grow: 1;
+    }
+
+    .view > header {
+        flex: 0 48px;
+    }
+
+    .view > footer {
+        flex: 0 64px;
+    }
+</style>
